eac3adf7e4078408aa7ebaf11addf08f57826744
Nikita Izmailov <nikitaizmailov@Nikitas-MacBook-Pro.local>
2020-03-31 21:50:30 +0300

    Changed some stuff again

--- django_project/settings.py
+++ django_project/settings.py
@@ -127,15 +127,12 @@ USE_L10N = True
 USE_TZ = True
 
 
-PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
