4b9776fada00297666fd7a72cc00fd96c0b5088e
Nikita Izmailov <nikitaizmailov@Nikitas-MacBook-Pro.local>
2020-03-31 20:57:32 +0300

    Added django-heroku

--- django_project/settings.py
+++ django_project/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import django_heroku
+
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -158,5 +160,5 @@ AWS_S3_SIGNATURE_VERSION = 's3v4'
 
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
-
+django_heroku.settings(locals())
 
